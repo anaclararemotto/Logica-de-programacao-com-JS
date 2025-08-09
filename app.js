8a8dfee06f268205046c694733174ec269b6a896
Clara <clararemotto@gmail.com>
2025-08-09 15:59:21 -0300

    Condicionais e concatenação

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-alert('Hello World!');
+// alert('Hello World!');
 
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10:');
+// let numeroSecreto = 5;
+// console.log(numeroSecreto);
 
-if (numeroSecreto == chute) {
-    console.log('Isso ai! Você descobriu o número secreto')
-} else {
-    console.log('Que pena! Você errou o número secreto');
-}
+// let chute = prompt('Escolha um número entre 1 e 10:');
+
+// //se o chute for igual ao numero secreto
+// if (numeroSecreto == chute) {
+//     // console.log('Isso ai! Você descobriu o número secreto');
+//     alert(`Isso ai! Você descobriu o número secreto, que era ${numeroSecreto}`);
+// } else {
+//     alert('Que pena! Você errou o número secreto :(');
+// }
 
 //EXERCICIOS
 // alert('Boas vindas ao nosso site!');
@@ -31,3 +35,34 @@ if (numeroSecreto == chute) {
 // if (idadeInputada >= 18) {
 //     alert('Já pode tirar a Habilitação')
 // }
+
+//-----------------------------------------------
+
+// let diaSemana = prompt("Que dia da semana é hoje?");
+// if(diaSemana == 'sabado'){
+//     alert(`Hoje é ${diaSemana}, então tenha um bom fim de semana!`);
+// } else if (diaSemana == 'domingo') {
+//     alert(`Hoje é ${diaSemana}, então tenha um bom fim de semana!`);
+// } else {   
+//     alert(`Hoje é ${diaSemana}, tenha uma boa semana!`);
+// }
+
+// let numero = prompt("Digite um numero:");
+// if (numero >= 0){
+//     alert(`${numero} é um número positivo!`);
+// } else {
+//     alert(`${numero} é um número negativo`);
+// }
+
+// let pontuação = 100;
+// if (pontuação >= 100) {
+//     alert(`Sua pontuação é ${pontuação}. Parabens, você venceu!`)
+// } else {
+//     alert(`Sua pontuação é ${pontuação}. Tente novamente para ganhar`)
+// }
+
+// let saldoConta = 5000;
+// alert(`Seu saldo em conta é de R$ ${saldoConta}.`);
+
+// let nomeUsuário = prompt('Qual é o seu nome?');
+// alert(`Boas vindas ${nomeUsuário}!`)
