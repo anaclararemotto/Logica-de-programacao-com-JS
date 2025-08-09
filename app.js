3be3c96e02d9f7d610245c26f02b521b1fd27373
Clara <clararemotto@gmail.com>
2025-08-09 14:49:43 -0300

    iniciando com JS

---
+++ app.js
@@ -0,0 +1,33 @@
+alert('Hello World!');
+
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10:');
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai! Você descobriu o número secreto')
+} else {
+    console.log('Que pena! Você errou o número secreto');
+}
+
+//EXERCICIOS
+// alert('Boas vindas ao nosso site!');
+
+// let nome = 'Lua';
+
+// let idade = 25;
+
+// let numeroDeVendas = 50;
+
+// let saldoDisponivel = 1000;
+
+// alert( 'Erro! Preencha todos os campos');
+
+// let mensagemDeErro = 'Erro! Preencha todos os campos'
+// alert(mensagemDeErro);
+
+// let nomeInputado = prompt('Qual é o seu nome?');
+
+// let idadeInputada = prompt('Qual é a sua idade?');
+// if (idadeInputada >= 18) {
+//     alert('Já pode tirar a Habilitação')
+// }
