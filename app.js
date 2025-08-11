4fadee75f4c1027f3bb096934c06b644a2f06fc6
Clara <clararemotto@gmail.com>
2025-08-11 10:39:40 -0300

    Boas praticas de programação

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 // alert('Hello World!');
 
-let numeroSecreto = 5;
-console.log(numeroSecreto);
+// let numeroSecreto = parseInt (Math.random() * 100 + 1);
+// console.log(numeroSecreto);
 
-let chute;
+// let chute;
 
-let tentativas = 1;
+// let tentativas = 1;
 
-//enquanto chute não for igual ao numero secreto
-while (numeroSecreto != chute) {
-  chute = prompt("Escolha um número entre 1 e 10:");
+// //enquanto chute não for igual ao numero secreto
+// while (numeroSecreto != chute) {
+//   chute = prompt("Escolha um número entre 1 e 100:");
 
-  //se o chute for igual ao numero secreto
-  if (numeroSecreto == chute) {
-    // console.log('Isso ai! Você descobriu o número secreto');
-    alert(
-      `Isso ai! Você descobriu o número secreto, que era ${numeroSecreto} com ${tentativas} tentativas`
-    );
-  } else {
-    if (numeroSecreto > chute) {
-      alert(`O numero secreto é maior que ${chute}`);
-    } else {
-      alert(`O numero secreto é menor que ${chute}`);
-    }
-    tentativas++;
-  }
-}
+//   //se o chute for igual ao numero secreto
+//   if (numeroSecreto == chute) {
+//     // console.log('Isso ai! Você descobriu o número secreto');
+//     break;
+//   } else {
+//     if (numeroSecreto > chute) {
+//       alert(`O numero secreto é maior que ${chute}`);
+//     } else {
+//       alert(`O numero secreto é menor que ${chute}`);
+//     }
+//     // tentativas = tentativas + 1
+//     tentativas++;
+//   }
+// }
+
+// let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+// alert(
+//   `Isso ai! Você descobriu o número secreto, que era ${numeroSecreto} com ${tentativas} ${palavraTentativa}`
+// );
+
+// if (tentativas > 1){
+//     alert(
+//           `Isso ai! Você descobriu o número secreto, que era ${numeroSecreto} com ${tentativas} tentativas`
+//         );
+
+// } else {
+//     alert(
+//           `Isso ai! Você descobriu o número secreto, que era ${numeroSecreto} com ${tentativas} tentativa`
+//         );
+// }
 
 //EXERCICIOS
 // alert('Boas vindas ao nosso site!');
@@ -111,3 +126,63 @@ while (numeroSecreto != chute) {
 //     console.log(contadorProgressivo);
 //     contadorProgressivo++;
 // }
+//-----------------------------------------------
+
+//DESAFIO FINAL
+
+console.log('Boas vindas!');
+
+let nome = 'Ana Clara';
+console.log(`Boas vindas ${nome}!`);
+
+alert(`Olá, ${nome}!`);
+
+let linguagemProgramacao = prompt('Qual é a linguagem de programação que você mais gosta?');
+console.log(linguagemProgramacao);
+
+let valor1 = 8;
+let valor2 = 6;
+let resultado = valor1 + valor2;
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+let resultado2 = valor1 - valor2;
+console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado2}`);
+
+let idade = prompt('Informe sua idade:');
+if (idade >= 18) {
+    console.log('Você é maior de idade');
+} else {
+    console.log('Você é menor de idade');
+}
+
+let numero = prompt('Informe um numero');
+if (numero == 0){
+    console.log('O numero é zero');
+} else if (numero > 0){
+    console.log(`O numero ${numero} é positivo`);
+} else{
+console.log(`O numero ${numero} é negativo`);
+}
+
+let contador = 1;
+while (contador <=10 ) {
+    console.log(contador);
+    contador++;
+}
+
+let nota = 7;
+if(nota >= 7){
+    console.log('Aprovado!');
+} else {
+    console.log('Reprovado!');
+}
+
+let numeroAleatorio = Math.random();
+console.log(numeroAleatorio);
+
+let numeroInteiro = parseInt (Math.random() * 10 + 1);
+console.log(`Numero inteiro de 1 a 10: ${numeroInteiro}`);
+
+let numeroInteiro2 = parseInt (Math.random() * 1000 + 1);
+console.log(`Numero inteiro de 1 a 1000: ${numeroInteiro2}`);
+
