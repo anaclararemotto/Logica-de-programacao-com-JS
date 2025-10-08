594973b933a7a7c5d3ac043371258b0f97589657
Clara <clararemotto@gmail.com>
2025-10-08 10:58:11 -0300

    reiniciando o jogo

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -22,3 +22,86 @@ function botaoSoma() {
     let resultado = valor1 + valor2;
     alert(`o resultado da soma de ${valor1} e ${valor2} é igual a ${resultado}`)
 }
+
+function calculaIMC(altura, peso) {
+  imc = peso / (altura * altura);
+
+  console.log(`Seu IMC é equivalente a: ${imc}`);
+}
+
+calculaIMC(1.9, 80);
+
+function calculaFatorial(numero) {
+  if (numero === 0 || numero === 1) {
+    return 1;
+  }
+
+  let fatorial = 1;
+  for (let i = 2; i <= numero; i++) {
+    fatorial *= i;
+  }
+
+  return fatorial;
+}
+
+let numero = 5;
+let resultado = calculaFatorial(numero);
+console.log(`O fatorial de ${numero} é ${resultado}`);
+
+function converteParaDolar(dolar, valor) {
+  resultado = dolar * valor;
+}
+
+let dolar = 4.8;
+let valor = 5;
+let resultadoConvercao = converteParaDolar(dolar, valor);
+console.log(`${valor} é igual a ${resultado} dolares`);
+
+// function converterDolarParaReal(valorEmDolar) {
+//   let cotacaoDolar = 4.80;
+//   let valorEmReais = valorEmDolar * cotacaoDolar;
+//   return valorEmReais.toFixed(2);
+// }
+
+// // Exemplo de uso
+// let valorEmDolar = 50;
+// let valorEmReais = converterDolarParaReal(valorEmDolar);
+// console.log(`${valorEmDolar} dólares equivalem a R$ ${valorEmReais}`);
+
+function calcularAreaPerimetroRetangulo(altura, largura) {
+  let area = largura * altura;
+  let perimetro = 2 * (largura + altura);
+
+  console.log(`Área da sala: ${area} metros quadrados`);
+  console.log(`Perímetro da sala: ${perimetro} metros`);
+}
+
+let altura = 5;
+let largura = 3;
+
+calcularAreaPerimetroRetangulo(altura, largura);
+
+function calcularAreaPerimetroCirculo(raio) {
+  let pi = 3.14;
+
+  let perimetro = 2 * pi * raio;
+  let area = Math.PI * raio * raio;
+
+  console.log(`Área da sala circular: ${area.toFixed(2)} metros quadrados`);
+  console.log(`Perímetro da sala circular: ${perimetro.toFixed(2)} metros`);
+}
+
+raio = 5;
+calcularAreaPerimetroCirculo(raio);
+
+function mostrarTabuada(numero) {
+    // i = 1; enquanto i for menor ou igual a 10, i incrementa
+  for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} X ${i} = ${resultado}`);
+    
+  }
+}
+
+let numeroTabuada = 5;
+mostrarTabuada(numeroTabuada)
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,35 +1,34 @@
-
 function saudacao() {
-    console.log('Olá mundo');
-    
+  console.log("Olá mundo");
 }
-saudacao()
+saudacao();
 
-function exibirNome(nome){
-    console.log(`Olá, ${nome}`)
+function exibirNome(nome) {
+  console.log(`Olá, ${nome}`);
 }
-exibirNome('clara')
+exibirNome("clara");
 
-function calcularDobro(numero){
-    return numero *2
+function calcularDobro(numero) {
+  return numero * 2;
 }
 let resultadoDobro = calcularDobro(8);
 console.log(resultadoDobro);
 
 function calculaMedia(num1, num2, num3) {
-    return (num1 + num2 + num3) / 3;
+  return (num1 + num2 + num3) / 3;
 }
-let resultadoMedia = calculaMedia(5,8,9);
+let resultadoMedia = calculaMedia(5, 8, 9);
 console.log(resultadoMedia);
 
-function encontraMaior(num1, num2){
-    return num1 > num2 ? num1 : num2;
+function encontraMaior(num1, num2) {
+  return num1 > num2 ? num1 : num2;
 }
 let resultadoMaior = encontraMaior(2, 15);
 console.log(resultadoMaior);
 
 function calculaQuadrado(numero) {
-    return numero * numero;  
+  return numero * numero;
 }
 let resultadoQuadrado = calculaQuadrado(7);
 console.log(resultadoQuadrado);
+
